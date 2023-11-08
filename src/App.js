43472125f6ade9af88f60cd97ef03a00eeb7ec7a
bhuwan <bkspoudel@gmail.com>
2023-11-08 20:13:31 +0100

    fix setup for custom hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// https://abcd-d4c9f-default-rtdb.europe-west1.firebasedatabase.app/tasks.json
+
+import "./App.css";
+
+const App = () => {
+  return <div className="container">App</div>;
+};
 
 export default App;
